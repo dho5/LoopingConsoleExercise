533811b5ec6fd182eccb2a2c7c4291200feb3d3c
dho5 <55501320+dho5@users.noreply.github.com>
2019-12-09 09:31:35 -0700

    Add files via upload

---
+++ js-looping-console-exercise/main.js
@@ -0,0 +1,31 @@
+// Looping Console Exercise
+'use strict';
+
+// Exercise #1
+for (let i = 0; i < 500; i++) {
+  console.log("I'm so happy!");
+}
+
+// Exercise #2
+for (let i = 12; i <= 800; i += 4) {
+  console.log(i);
+}
+
+// Exercise #3
+for (let n = 55; n >= 11; n -= 2) {
+  console.log(n);
+}
+
+// Exercise #4
+let sum = 0;
+for (let x = 5; x <= 50; x++) {
+  sum += x;
+}
+console.log('Sum of series is: ' + sum);
+
+// Exercise #5
+let seriesSum = 0;
+for (let y = 10; y <= 100; y += 10) {
+  seriesSum += y;
+}
+console.log('Sum of series is: ' + seriesSum);
